98d55d9051031448e72262bc533fe4c9edb246bb
takumi-hirokane <plusastinc@gmail.com>
2019-12-13 17:15:39 +0000

    user login

--- app/User.php
+++ app/User.php
@@ -14,10 +14,17 @@ class User extends Authenticatable
      *
      * @var array
      */
+     
+      
+        
+        
+        
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
+
+   
     /**
      * The attributes that should be hidden for arrays.
      *
